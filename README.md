718395b0e5e76443182801398f0ef292e65245e0
Nans Lefebvre <len@odoo.com>
2019-10-20 21:15:47 +0200

    [IMP] document a bit the app

--- README.md
+++ README.md
@@ -1,7 +1,17 @@
 # findR
 findRestaurants REST API
 
-TODO:
+Return a list of 10 nearby restaurants for a given location.
+Optional arguments include list of preferred categories for attendees, the maximal distance, etc.
+The full input request schema (as well as the output) is defined in validate.
+The list of categories can be found in data/categories.
+
+The requestr should handle the app logic, and manage the different restaurant providers;
+there is only Yelp for now, so the API closely mirrors it.
+
+To generate example curl commands, head to generate_curl_command (pretty explicit right?).
+
+# TODO:
  - authentication
  - ranking
  - localize error messages
---
+++ generate_curl_command.py
@@ -0,0 +1,22 @@
+from urllib import parse
+import json
+
+data = {
+            'location': {
+                'name': 'Flagey Building 29, rue du Belvédère 1050 Brussels Belgium',
+            },
+            'attendees': [
+                {'name': 'Gilles', 'preferences': 'afghani,african,bbq'},
+                {'name': 'Vincent', 'preferences': 'asianfusion,burgers,brasseries'},
+                {'name': 'Edwin', 'preferences': ''}
+            ],
+            'max_distance': 1000,
+            'price_range': "1,2",
+            'locale': 'en_BE',
+            'open_now': True
+}
+
+args = parse.urlencode({'data': json.dumps(data)})
+
+# print("curl -X GET 'http://localhost:5000/findr?'" + args)  # dev
+print("curl -X GET 'https://lunchpickr.herokuapp.com/findr?'" + args)
